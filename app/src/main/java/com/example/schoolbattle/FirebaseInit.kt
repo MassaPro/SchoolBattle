dd6e29a374814908b7de66cd23e7da3ba7e5ce69
MassaPro <63496032+MassaPro@users.noreply.github.com>
2020-05-03 17:38:50 +0300

    NULL-version of our project

---
+++ app/src/main/java/com/example/schoolbattle/FirebaseInit.kt
@@ -0,0 +1,8 @@
+package com.example.schoolbattle
+
+import com.google.firebase.database.FirebaseDatabase
+
+//Здесь объявлена наша база данных
+
+var database = FirebaseDatabase.getInstance()   
+//var myRef = database.getReference("message")
