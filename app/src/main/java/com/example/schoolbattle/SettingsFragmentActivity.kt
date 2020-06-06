256272e1a134d4ed045ed1bfd85d5d9cf17e20c5
Denis <denis.spb02@mail.ru>
2020-06-06 23:55:01 +0300

    Добавил панель навигации в играх

--- app/src/main/java/com/example/schoolbattle/SettingsFragmentActivity.kt
+++ app/src/main/java/com/example/schoolbattle/SettingsFragmentActivity.kt
@@ -1,9 +1,12 @@
 package com.example.schoolbattle
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CompoundButton
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.activity_settings_fragment.*
@@ -19,9 +22,40 @@ class SettingsFragmentActivity : Fragment() {
         return inflater.inflate(R.layout.activity_settings_fragment, container, false)
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        //(activity as AppCompatActivity?)!!.setSupportActionBar(toolbar)
-        //setSupportActionBar(toolbar)
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        (activity as AppCompatActivity?)!!.setSupportActionBar(my_toolbar_settings)
+
+
+        logOutSettings.setOnClickListener {
+            val editor = activity?.getSharedPreferences("UserData", Context.MODE_PRIVATE)?.edit()
+            editor?.putString("username", "")
+            editor?.apply()
+            recyclerSet.clear()
+            val intent = Intent(activity, NullActivity::class.java)
+            startActivity(intent)
+            activity?.finish()
+        }
+        soundSwitch.isChecked = true
+
+        soundSwitch.setOnCheckedChangeListener(CompoundButton.OnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                //
+            } else {
+                //
+            }
+        })
+
+
     }
+    /*override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //(activity as AppCompatActivity?)!!.setSupportActionBar(my_toolbar)
+        //setSupportActionBar(my_toolbar)
+
+
+
+
+    }*/
 }
--- app/src/main/java/com/example/schoolbattle/SnakeGameWithComputer.kt
+++ app/src/main/java/com/example/schoolbattle/SnakeGameWithComputer.kt
@@ -1,12 +1,42 @@
 package com.example.schoolbattle
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_snake_game_with_computer.*
 
 class SnakeGameWithComputer : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_snake_game_with_computer)
+
+
+
+        bottom_navigation_snake_with_computer.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.page_1 ->{
+
+                }
+                R.id.page_2 ->{
+                    //dialog_parametrs = Show_parametr_with_computer(this@SnakeGameWithComputer)
+                    //dialog_parametrs?.showResult_with_computer(this)
+                }
+                R.id.page_3 ->{
+                    this.finish()
+                    val intent = Intent(this, SnakeGameWithComputer::class.java).apply {
+                        putExtra("usedToClear", "clear")
+                    }
+                    startActivity(intent)
+                }
+                R.id.page_4 ->{
+
+                }
+
+            }
+            true
+        }
+
     }
 }
