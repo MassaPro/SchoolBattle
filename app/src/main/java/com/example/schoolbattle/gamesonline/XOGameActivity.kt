48c657702c2921ed12521a962374023bfe4c9fb6
Нуртдинов Альфред <nurtdinov17@inbox.ru>
2020-06-11 03:57:26 +0300

    add prototype of disign in xog_online

--- app/src/main/java/com/example/schoolbattle/gamesonline/XOGameActivity.kt
+++ app/src/main/java/com/example/schoolbattle/gamesonline/XOGameActivity.kt
@@ -3,6 +3,8 @@ package com.example.schoolbattle.gamesonline
 import android.app.Activity
 import android.content.Context
 import android.graphics.*
+import android.graphics.Color.argb
+import android.graphics.Color.rgb
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.AttributeSet
@@ -14,6 +16,7 @@ import com.example.schoolbattle.*
 import com.example.schoolbattle.R
 import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_x_o_game.*
+import kotlinx.android.synthetic.main.activity_x_o_game_one_divice.*
 
 class XOGameActivity : AppCompatActivity() {
     private var isRun = false
@@ -29,6 +32,23 @@ class XOGameActivity : AppCompatActivity() {
     override fun onCreate(savedInstance: Bundle?) {
         super.onCreate(savedInstance)
         setContentView(R.layout.activity_x_o_game)
+
+        if(Design == "Egypt" ) {
+            button_player_1_online_xog.setTextColor(Color.BLACK)
+            button_player_2_online_xog.setTextColor(Color.BLACK)
+            button_player_1_online_xog.setTextSize(10f)
+            button_player_2_online_xog.setTextSize(10f)
+
+            icon_player_1_xog_online.setBackgroundResource(R.drawable.player1_egypt);
+            icon_player_2_xog_online.setBackgroundResource(R.drawable.player2_egypt);
+            player_1_icon_xog_online.setBackgroundResource(R.drawable.cross_egypt);
+            player_2_icon_xog_online.setBackgroundResource(R.drawable.circle_egypt);
+            label_online_xog.setBackgroundResource(R.drawable.background_egypt);
+            bottom_navigation_xog_online.setBackgroundColor(rgb(224,164,103))
+            to_back_xog_online.setBackgroundResource(R.drawable.arrow_back)
+            toolbar_xog_online.setBackgroundColor(argb(0,0,0,0))
+            toolbar2_xog_online.setBackgroundColor(argb(0,0,0,0))
+        }
 
         currentContext = this
         CONTEXT = this
@@ -64,8 +84,10 @@ class XOGameActivity : AppCompatActivity() {
             override fun onDataChange(p0: DataSnapshot) {
                 var cnt = 0
                 signature_canvas.isFirstMove = (p0.child("Move").value.toString() == yu.toString())
-                up_player.text = yourName + if (signature_canvas.isFirstMove) " X" else " O"
-                down_player.text = opponentsName + if (!signature_canvas.isFirstMove) " X" else " O"
+                button_player_1_online_xog.text = yourName
+                        //if (signature_canvas.isFirstMove) " X" else " O"
+                button_player_2_online_xog.text = opponentsName
+                        //if (!signature_canvas.isFirstMove) " X" else " O"
                 for (i in 0..6) {
                     for (j in 0..5) {
                         val p = p0.child("$i").child("$j")
@@ -197,12 +219,25 @@ class XOGameActivity : AppCompatActivity() {
 
 class CanvasView(context: Context, attrs: AttributeSet?) : View(context, attrs) {
 
-    fun touch_refinement_X (x : Float,width1: Float,size_field_x1:Int ):Float        //уточняет касания по оси x
+    fun touch_refinement_X (indent: Float,x : Float,width1: Float,size_field_x1:Int ):Float        //уточняет касания по оси x
     {
-        return ((x.toInt()/(width1/size_field_x1).toInt()).toFloat()*width1/size_field_x1).toFloat()
+        if(x<indent)
+        {
+            return -1f
+        }
+        if(x>width1-indent)
+        {
+            return -1f
+        }
+        var a : Float = indent
+        while(x>a)
+        {
+            a+=step
+        }
+        return a - step
     }
 
-    fun touch_refinement_Y (y : Float,height1: Float,size_field_y1:Int,step: Float,advertising_line_1:Float):Float      //уточняет касания по оси Y
+    fun touch_refinement_Y (indent: Float,y : Float,height1: Float,size_field_y1:Int,step: Float,advertising_line_1:Float):Float      //уточняет касания по оси Y
     {
         if(y > height1 - advertising_line_1 ||  y < height1 - advertising_line_1 - step*size_field_y1)
         {
@@ -216,16 +251,16 @@ class CanvasView(context: Context, attrs: AttributeSet?) : View(context, attrs) 
         return a - step
     }
 
-    fun touch_refinement_for_Array_X (x : Float,step:Float):Int        //уточняет координаты в массиве  при касании
+    fun touch_refinement_for_Array_X (indent: Float,x : Float,step:Float):Int        //уточняет координаты в массиве  при касании
     {
         if(x<0)
         {
             return -1
         }
-        return (x/step).toInt()
+        return ((x-indent)/step).toInt()
     }
 
-    fun touch_refinement_for_Array_Y (y : Float,height1: Float,size_field_y1: Int,step: Float,advertising_line_1:Float):Int      //уточняет координаты в массиве  при касании
+    fun touch_refinement_for_Array_Y (indent: Float,y : Float,height1: Float,size_field_y1: Int,step: Float,advertising_line_1:Float):Int      //уточняет координаты в массиве  при касании
     {
         if(y<0)
         {
@@ -241,12 +276,12 @@ class CanvasView(context: Context, attrs: AttributeSet?) : View(context, attrs) 
         return b-1
     }
 
-    private fun translate_from_Array_to_Graphics_X(x:Int, step: Float):Float    //переводит массивные координаты в графически
+    private fun translate_from_Array_to_Graphics_X(indent: Float,x:Int, step: Float):Float    //переводит массивные координаты в графически
     {
-        return x*step
+        return x*step+indent
     }
 
-    fun translate_from_Array_to_Graphics_Y(y:Int,height1: Float,size_field_y1: Int,step: Float,advertising_line_1: Float):Float    //переводит массивные координаты в графически
+    fun translate_from_Array_to_Graphics_Y(indent: Float,y:Int,height1: Float,size_field_y1: Int,step: Float,advertising_line_1: Float):Float    //переводит массивные координаты в графически
     {
         return y*step + height1 - size_field_y1*step - advertising_line_1
     }
@@ -288,11 +323,14 @@ class CanvasView(context: Context, attrs: AttributeSet?) : View(context, attrs) 
     var circlex : Float = 0f   //координаты нажатия
     var circley : Float = 0f
 
+    var indent : Float = 20f
+
     var paint : Paint = Paint()          //ресурсы для рисования
     var Line_paint: Paint = Paint()
     var Line_paint_1: Paint = Paint()
     var FIELD = Array(7){IntArray(6)}
     var cross_or_nul: String
+    var step: Float = 0f
 
     init {
         Exit = 0
@@ -324,55 +362,76 @@ class CanvasView(context: Context, attrs: AttributeSet?) : View(context, attrs) 
 
 
 
-    var icon_cross : Bitmap = BitmapFactory.decodeResource(context.getResources(),
+    var icon_cross_egypt : Bitmap = BitmapFactory.decodeResource(context.getResources(),
         R.drawable.cross_egypt
     )       //картинки крестиков и нулей
-    var icon_null: Bitmap = BitmapFactory.decodeResource(context.getResources(),
-        R.drawable.circle_null
+    var icon_null_egypt: Bitmap = BitmapFactory.decodeResource(context.getResources(),
+        R.drawable.circle_egypt
     )
 
-    var icon_green:  Bitmap = BitmapFactory.decodeResource(context.getResources(),
-        R.drawable.green_icon
+    // var BackgroundColor_Egypt: Bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.background_egypt)
+    var icon_green : Bitmap = BitmapFactory.decodeResource(context.getResources(),
+        R.drawable.illumination
     )
+    var icon_grenn_Egypt : Bitmap = BitmapFactory.decodeResource(context.getResources(),
+        R.drawable.ram_egypt_xog
+    )
+
 
     override fun draw(canvas: Canvas?) {
         super.draw(canvas)
 
         //TODO() take field from database
-        canvas?.drawColor(Color.WHITE)
+
+        indent = 20f
         val width = getWidth().toFloat()
         val height = getHeight().toFloat()
         //ширина и высота экрана (от ширины в основном все зависит)
 
         val size_field_x: Int = 7
         val size_field_y: Int = 6
-        val step: Float = width/size_field_x
+        step = (width-2*indent)/size_field_x
         val advertising_line: Float = (height - step * 6) / 2
 
-        var k: Float = height-width-advertising_line + step
-        for(i in 0 until size_field_x)
+        var k = height - (width-2*indent)*size_field_y/size_field_x - advertising_line
+        for(i in 0 until 7)          //вырисовка горизонтальных линий
         {
-            canvas?.drawLine(0f,k,width,k,Line_paint)
-            k += step
+            canvas?.drawLine(indent,k,width-indent,k,Line_paint)
+            k = k + step
         }
-        k = 0f
-        for(i in 0 until size_field_y+2)
+        k  =  height-(width-2*indent)*size_field_y/size_field_x-advertising_line
+        var t = indent
+        for(i in 0 until 8)         //вырисовка вертикальных линий
         {
-            if(i == 0 || i == size_field_y+1)
-            {
-                canvas?.drawLine(k,height-advertising_line-width+step,k,height-advertising_line,Line_paint_1)
-            }
-            else
-            {
-                canvas?.drawLine(k,height-advertising_line-width+step,k,height-advertising_line,Line_paint)
-            }
-            k += step
+            canvas?.drawLine(t,k,t,height-advertising_line,Line_paint)
+            t = t + step
         }
 
 
-        val right_icon_cross: Bitmap = Bitmap.createScaledBitmap(icon_cross,width.toInt()/size_field_x, width.toInt()/size_field_x, true); //подгоняем картинку под размеры экрана телефона
-        val right_icon_null: Bitmap = Bitmap.createScaledBitmap(icon_null,width.toInt()/size_field_x, width.toInt()/size_field_x, true);
-        val right_icon_green: Bitmap = Bitmap.createScaledBitmap(icon_green,width.toInt()/size_field_x, width.toInt()/size_field_x, true);
+        val right_icon_cross: Bitmap  //подгоняем картинку под размеры экрана телефона
+        val right_icon_null: Bitmap
+        val right_icon_green: Bitmap
+        if(Design == "Egypt")
+        {
+            right_icon_cross = Bitmap.createScaledBitmap(icon_cross_egypt,(width.toInt()-2*indent.toInt())/size_field_x, (width.toInt()-2*indent.toInt())/size_field_x, true);
+            right_icon_null = Bitmap.createScaledBitmap(icon_null_egypt,(width.toInt()-2*indent.toInt())/size_field_x, (width.toInt()-2*indent.toInt())/size_field_x, true);
+            right_icon_green = Bitmap.createScaledBitmap(
+                icon_grenn_Egypt,
+                (width.toInt() - 2 * indent.toInt()) / size_field_x,
+                (width.toInt() - 2 * indent.toInt()) / size_field_x,
+                true
+            )
+        }
+        else {
+            right_icon_cross = Bitmap.createScaledBitmap(icon_cross_egypt,(width.toInt()-2*indent.toInt())/size_field_x, (width.toInt()-2*indent.toInt())/size_field_x, true);
+            right_icon_null = Bitmap.createScaledBitmap(icon_null_egypt,(width.toInt()-2*indent.toInt())/size_field_x, (width.toInt()-2*indent.toInt())/size_field_x, true);
+            right_icon_green = Bitmap.createScaledBitmap(
+                icon_green,
+                (width.toInt() - 2 * indent.toInt()) / size_field_x,
+                (width.toInt() - 2 * indent.toInt()) / size_field_x,
+                true
+            )
+        }
 
         var cnt = 0
         for( i in 0..6) //начальная расстановка крестиков и ноликов
@@ -381,14 +440,14 @@ class CanvasView(context: Context, attrs: AttributeSet?) : View(context, attrs) 
                 if (FIELD[i][j] == 1)  //крестик
                 {
                     cnt++
-                    canvas?.drawBitmap(right_icon_cross, translate_from_Array_to_Graphics_X(i,step),
-                        translate_from_Array_to_Graphics_Y(j,height,size_field_y,step,advertising_line),paint)
+                    canvas?.drawBitmap(right_icon_cross, translate_from_Array_to_Graphics_X(indent,i,step),
+                        translate_from_Array_to_Graphics_Y(indent,j,height,size_field_y,step,advertising_line),paint)
                 }
                 if (FIELD[i][j] == 2)  //нолик
                 {
                     cnt--
-                    canvas?.drawBitmap(right_icon_null, translate_from_Array_to_Graphics_X(i,step),
-                        translate_from_Array_to_Graphics_Y(j,height,size_field_y,step,advertising_line),paint)
+                    canvas?.drawBitmap(right_icon_null, translate_from_Array_to_Graphics_X(indent,i,step),
+                        translate_from_Array_to_Graphics_Y(indent,j,height,size_field_y,step,advertising_line),paint)
                 }
             }
         }
@@ -399,10 +458,10 @@ class CanvasView(context: Context, attrs: AttributeSet?) : View(context, attrs) 
             "null"
         }
 
-        if (touch_refinement_Y(circley,height,size_field_y,step,advertising_line)>0)     //постановка нового обЪекта
+        if (touch_refinement_Y(indent,circley,height,size_field_y,step,advertising_line)>0)     //постановка нового обЪекта
         {
-            val X: Int = touch_refinement_for_Array_X(circlex,step)
-            val Y: Int = touch_refinement_for_Array_Y(circley,height,size_field_y,step,advertising_line)    //координаты нажимаего для массива
+            val X: Int = touch_refinement_for_Array_X(indent,circlex,step)
+            val Y: Int = touch_refinement_for_Array_Y(indent,circley,height,size_field_y,step,advertising_line)    //координаты нажимаего для массива
 
             if (moveChecker(X, Y, cnt) && FIELD[X][Y]==0)
             {
@@ -411,8 +470,8 @@ class CanvasView(context: Context, attrs: AttributeSet?) : View(context, attrs) 
                 var b:Float = circley
                 if(cross_or_nul=="cross")
                 {
-                    canvas?.drawBitmap(right_icon_cross,touch_refinement_X(a,width,size_field_x),
-                        touch_refinement_Y(b,height,size_field_y,step,advertising_line),paint)
+                    canvas?.drawBitmap(right_icon_cross,touch_refinement_X(indent,a,width,size_field_x),
+                        touch_refinement_Y(indent,b,height,size_field_y,step,advertising_line),paint)
                     FIELD[X][Y] = 1
                     positionData.child("$X").child("$Y").setValue(1)
                     cross_or_nul = "null"
@@ -420,8 +479,8 @@ class CanvasView(context: Context, attrs: AttributeSet?) : View(context, attrs) 
                 }
                 else
                 {
-                    canvas?.drawBitmap(right_icon_null,touch_refinement_X(a,width,size_field_x),
-                        touch_refinement_Y(b,height,size_field_y,step,advertising_line),paint)
+                    canvas?.drawBitmap(right_icon_null,touch_refinement_X(indent,a,width,size_field_x),
+                        touch_refinement_Y(indent,b,height,size_field_y,step,advertising_line),paint)
                     FIELD[X][Y] = 2
                     positionData.child("$X").child("$Y").setValue(2)
                     //TODO setValue to database
@@ -436,10 +495,10 @@ class CanvasView(context: Context, attrs: AttributeSet?) : View(context, attrs) 
             var counter: Int = 1
             while(counter<9)
             {
-                var a_1: Float =  translate_from_Array_to_Graphics_X(checkForWin_another_fun()[counter],step)
+                var a_1: Float =  translate_from_Array_to_Graphics_X(indent,checkForWin_another_fun()[counter],step)
 
 
-                var a_2: Float = translate_from_Array_to_Graphics_Y(checkForWin_another_fun()[counter+1].toInt(), height,size_field_y, step, advertising_line)
+                var a_2: Float = translate_from_Array_to_Graphics_Y(indent,checkForWin_another_fun()[counter+1].toInt(), height,size_field_y, step, advertising_line)
                 canvas?.drawBitmap(right_icon_green,a_1,a_2,paint)
                 counter += 2
             }
