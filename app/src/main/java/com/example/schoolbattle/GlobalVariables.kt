412bc2c00bd67b89a4fab6409be496a348bf17b9
EuphoriaAbsorber <scorpion1remeres@gmail.com>
2021-01-28 23:00:31 +0300

    цены

--- app/src/main/java/com/example/schoolbattle/GlobalVariables.kt
+++ app/src/main/java/com/example/schoolbattle/GlobalVariables.kt
@@ -35,13 +35,13 @@ var SOUND: Boolean = false
 var VIBRATION: Boolean = false
 //____________________________________________________________________________________________________________________________
 var Design: String = "Normal"               //дизайн
-val PICTURE_STYLES = mapOf(0 to R.drawable.game_menu_normal, 1 to R.drawable.game_menu_egypt, 2 to R.drawable.game_menu_casino,3 to R.drawable.game_menu_rome,4 to R.drawable.game_menu_gothic,
-    5 to R.drawable.game_menu_japan, 6 to R.drawable.game_menu_noir)
-val PICTURE_TEXT = mapOf(0 to "Деловой стиль", 1 to "Eгипетскй стиль", 2 to "Казино стиль",3 to "Римский стиль",4 to "Готический стиль",5 to "Японский стиль", 6 to "Нуар стиль")
-val PRICE_OD_DESIGN = mapOf(0 to 10,1 to 20,2 to 30,3 to 1,4 to 1, 5 to 3, 6 to 10)
+val PICTURE_STYLES = mapOf(0 to R.drawable.game_menu_normal, 1 to R.drawable.game_menu_noir, 2 to R.drawable.game_menu_rome,3 to R.drawable.game_menu_casino,4 to R.drawable.game_menu_egypt,
+    5 to R.drawable.game_menu_gothic, 6 to R.drawable.game_menu_japan)
+val PICTURE_TEXT = mapOf(0 to "Деловой стиль", 1 to "Нуар стиль", 2 to "Римский стиль", 3 to "Казино стиль", 4 to "Eгипетский стиль", 5 to "Готический стиль",6 to "Японский стиль")
+val PRICE_OD_DESIGN = mapOf(0 to 10,1 to 250,2 to 1000,3 to 10000,4 to 70000, 5 to 800000, 6 to 2000000)
 var ARRAY_OF_DESIGN_SHOP: MutableList<Int>  = mutableListOf(0,1,2,3,4,5,6)             //номера  дизайнов в магазине
-var ARRAY_OF_DESIGN: MutableList<Int>  = mutableListOf(0,1)             //номера открытых дизайнов
-var AUXILIARY_MAP_OF_DESIGNS = mapOf(0 to "Normal", 1 to "Egypt", 2 to "Casino",3 to "Rome", 4 to "Gothic", 5 to "Japan", 6 to "Noir")
+var ARRAY_OF_DESIGN: MutableList<Int>  = mutableListOf(0)             //номера открытых дизайнов
+var AUXILIARY_MAP_OF_DESIGNS = mapOf(0 to "Normal", 1 to "Noir", 2 to "Rome",3 to "Casino", 4 to "Egypt", 5 to "Gothic", 6 to "Japan")
 //__________________________________________________________________________________________________________________________________
 
 //____________________________________________________________________________________________________________________________
@@ -51,16 +51,16 @@ val PICTURE_AVATAR = mapOf(0 to R.drawable.av0, 1 to R.drawable.av1, 2 to R.draw
     14 to R.drawable.av14, 15 to R.drawable.av15, 16 to R.drawable.av16,17 to R.drawable.av17,18 to R.drawable.av18,19 to R.drawable.av19,20 to R.drawable.av20,
     21 to R.drawable.av21, 22 to R.drawable.av22, 23 to R.drawable.av23,24 to R.drawable.av24,25 to R.drawable.av25,26 to R.drawable.av26,27 to R.drawable.av27,
     28 to R.drawable.av28, 29 to R.drawable.av29, 30 to R.drawable.av30)
-val AVATAR_TEXT = mapOf(0 to "Мир", 15 to "Звезда 1", 20 to "Золото",40 to "Корона",40 to "Инь-Янь",70 to "Солнце",110 to "Меркурий",
+val AVATAR_TEXT = mapOf(0 to "Мир", 1 to "Звезда 1", 2 to "Золото",3 to "Корона",4 to "Инь-Янь",5 to "Солнце",6 to "Меркурий",
     7 to "Венера", 8 to "Земля", 9 to "Луна",10 to "Марс",11 to "Юпитер",12 to "Сатурн",13 to "Уран",
     14 to "Нептун", 15 to "Плутон", 16 to "Пентакль",17 to "Треугольник",18 to "Крест",19 to "Звезда 2",20 to "Тигр",
-    21 to "Пентаграмма", 22 to "Змея", 23 to "Маска",24 to "Топоры",25 to "Череп",7800 to "Лилия",10000 to "Роза",
-    10000 to "Сердце", 10000 to "Солнце", 23900 to "Шут")
-val PRICE_OD_AVATAR = mapOf(0 to 0, 1 to 0, 2 to 20,3 to 100,4 to 0,5 to 10,6 to 10,
-    7 to 10, 8 to 10, 9 to 10,10 to 10,11 to 20,12 to 20,13 to 20,
-    14 to 20, 15 to 20, 16 to 30,17 to 30,18 to 0,19 to 10,20 to 50,
-    21 to 50, 22 to 20, 23 to 30,24 to 60,25 to 60,26 to 100,27 to 50,
-    28 to 50, 29 to 100, 30 to 1)
+    21 to "Пентаграмма", 22 to "Змея", 23 to "Маска",24 to "Топоры",25 to "Череп",26 to "Лилия",27 to "Роза",
+    28 to "Сердце", 29 to "Солнце", 30 to "Шут")
+val PRICE_OD_AVATAR = mapOf(0 to 0, 1 to 70000, 2 to 100000,3 to 200000,4 to 40000,5 to 80,6 to 100,
+    7 to 120, 8 to 140, 9 to 160,10 to 180,11 to 200,12 to 220,13 to 240,
+    14 to 260, 15 to 400, 16 to 800,17 to 900,18 to 800,19 to 1000,20 to 3000,
+    21 to 2000, 22 to 2500, 23 to 5000,24 to 6000,25 to 10000,26 to 30000,27 to 15000,
+    28 to 20000, 29 to 50000, 30 to 1000000)
 var ARRAY_OF_AVATAR_SHOP: MutableList<Int>  = mutableListOf(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30)             //номера  дизайнов в магазине
 var ARRAY_OF_AVATAR: MutableList<Int>  = mutableListOf(0,1,4,18,19)             //номера открытых дизайнов
 var AUXILIARY_MAP_OF_AVATAR = mapOf(0 to "ava1", 1 to "ava2", 2 to "ava3",3 to "ava4")
@@ -71,7 +71,7 @@ var EMOTION: Int =  -1               //ЭМОЦИИ
 val PICTURE_EMOTION = mapOf(0 to R.drawable.e0, 1 to R.drawable.e1, 2 to R.drawable.e2,3 to R.drawable.e3,4 to R.drawable.e4,
    5 to R.drawable.e5,6 to R.drawable.e6,7 to R.drawable.e7,8 to R.drawable.e8,9 to R.drawable.e9)
 val EMOTION_TEXT = mapOf(0 to "Скукота", 1 to "Хохот", 2 to "Злость")
-val PRICE_OD_EMOTION = mapOf(0 to 10,1 to 20,2 to 30,3 to 90,4 to 1,5 to 1,6 to 1,7 to 1,8 to 1,9 to 1)
+val PRICE_OD_EMOTION = mapOf(0 to 100,1 to 200,2 to 400,3 to 0,4 to 3000,5 to 1000,6 to 5000,7 to 7500,8 to 8000,9 to 10000)
 var ARRAY_OF_EMOTION_SHOP: MutableList<Int>  = mutableListOf(0,1,2,3,4,5,6,7,8,9)             //номера  дизайнов в магазине
 var ARRAY_OF_EMOTION: MutableList<Int>  = mutableListOf(0,1)             //номера открытых эмоций
 var AUXILIARY_MAP_OF_EMOTION = mapOf(0 to "e0", 1 to "e1", 2 to "e2",3 to "e3",4 to "e4",5 to "e5",6 to "e6",7 to "e7",8 to "e8",9 to "e9")
